ab9879fc025cdff3efe8117920ef625d684888e6
SerBay <79163956434@yandex.ru>
2023-01-08 22:26:21 +0300

    final

--- lib/src/my_home_page.dart
+++ lib/src/my_home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/svg.dart';
 
 // Project imports:
 import 'package:skillbox_3/src/show_svg.dart';
@@ -17,6 +18,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final String localText = "SVG from local:";
+  final Widget svgLocal = SvgPicture.asset(
+    'assets/svg/spherical-maze.svg',
+    height: 300,
+  );
+
+  final String networkText = "SVG from network:";
+  final Widget svgNetwork = SvgPicture.network(
+    'https://raw.githubusercontent.com/dnfield/flutter_svg/7d374d7107561cbd906d7c0ca26fef02cc01e7c8/example/assets/flutter_logo.svg',
+    height: 300,
+    placeholderBuilder: (BuildContext context) => Container(
+        padding: const EdgeInsets.all(50.0),
+        child: const CircularProgressIndicator()),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
